8f2cc59cb851a90f5797650308c925295deaee5d
zeroxxl <zeroxxhls@gmail.com>
2025-02-13 12:29:36 +0100

    localestorage

--- src/components/Generate/QrCodeGenerator.jsx
+++ src/components/Generate/QrCodeGenerator.jsx
@@ -1,35 +1,49 @@
 import { useState } from 'react';
-import {QRCodeSVG} from 'qrcode.react';
-import s from './QrCodeGenerator.module.css'
+import { QRCodeSVG } from 'qrcode.react';
+import { GENERATE_DATA } from '../../constants';
+import s from './QrCodeGenerator.module.css';
 
-export const QrCodeGenerator =()=>{
+export const QrCodeGenerator = () => {
     const [value, setValue] = useState("");
-    const [result, setResult] = useState('');
+    const [result, setResult] = useState("");
 
-    const onClickHandler =()=>{
+    const onClickHandler = () => {
+        const prevData = JSON.parse(localStorage.getItem(GENERATE_DATA) || '[]');
+
+            localStorage.setItem(GENERATE_DATA, JSON.stringify([...prevData,value]));
+
         setResult(value);
         setValue('');
-    }
+    };
 
-    const onChangeHandler =(e)=>{
-       setValue(e.target.value);
-       setResult('')
-    }
+    const onChangeHandler = (e) => {
+        setValue(e.target.value);
+        setResult('');
+    };
 
-    return(
+    return (
         <div className={s.main_container}>
             <div className={s.content_card}>
                 <div className={s.card_tools}>
-                    <input placeholder='Введите текст...' type="text" value={value} onChange={onChangeHandler} className={s.result}/>
-                    <button type='button' onClick={onClickHandler} className={s.generator_btn}>
+                    <input 
+                        placeholder="Введите текст..." 
+                        type="text" 
+                        value={value} 
+                        onChange={onChangeHandler} 
+                        className={s.result} 
+                    />
+                    <button 
+                        type="button" 
+                        onClick={onClickHandler} 
+                        className={s.generator_btn}
+                    >
                         Generate
-                     </button>
+                    </button>
                 </div>
                 <div className={s.qrCode}>
-                    {result !== '' ? <QRCodeSVG value={result} size={150}/> : "no content"}
+                    {result !== '' ? <QRCodeSVG value={result} size={150} /> : "no content"}
                 </div>
             </div>
         </div>
-
     );
-}
+};
--- src/components/Scan/QrCodeScanner.jsx
+++ src/components/Scan/QrCodeScanner.jsx
@@ -1,5 +1,6 @@
 import { Scanner } from '@yudiel/react-qr-scanner';
 import { useState } from 'react';
+import { SCAN_DATA } from '../../constants';
 import s from './QrCodeScanner.module.css'
 
 export const QrCodeScanner =()=>{
@@ -7,6 +8,9 @@ const [scanned, setScanned] = useState(null)
 
     const scanHandler =(result)=>{
         setScanned(result[0].rawValue);
+        const prevData =JSON.parse(localStorage.getItem(SCAN_DATA)  || '[]')
+
+        localStorage.setItem(SCAN_DATA, JSON.stringify([...prevData, result[0].rawValue]))
     }
 
     const settings = {
@@ -22,7 +26,6 @@ const [scanned, setScanned] = useState(null)
         <div className={s.main_container}>
                 <div className={s.container}>
                 <Scanner
-                 allowMultiple
                  onScan={scanHandler}  
                  components={settings} 
                  styles={stylesSettings}
