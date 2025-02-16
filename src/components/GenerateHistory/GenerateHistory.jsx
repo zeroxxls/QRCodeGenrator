f2df892a9a3479a5f66e7b0ecb8a715550994c9a
zeroxxls <zeroxxhls@gmail.com>
2025-02-16 16:46:42 +0100

    final commit

--- src/components/GenerateHistory/GenerateHistory.jsx
+++ src/components/GenerateHistory/GenerateHistory.jsx
@@ -1,9 +1,24 @@
 import s from './GenerateHistory.module.css'
+import { GENERATE_DATA } from '../../constants'
+import { QRCodeSVG } from 'qrcode.react';
 
 export const GenerateHistory =()=>{
+
+    const data = JSON.parse(localStorage.getItem(GENERATE_DATA) || '[]')
+    console.log(data);
+
+
     return(
         <div>
-            test
+            <h1 className={s.list_tittle}>History</h1>
+            <div className={s.list}>
+            {data.map((text)=>(
+                <p key={text} className={s.list_elem}>
+                    {text}
+                    <QRCodeSVG value={text} size={100} />
+                    </p>
+            ))}
+            </div>
         </div>
     )
 }
--- src/components/ScanHistory/ScanHistory.jsx
+++ src/components/ScanHistory/ScanHistory.jsx
@@ -1,15 +1,23 @@
 import s from './ScanHistory.module.css'
 import { SCAN_DATA } from '../../constants';
+import { QRCodeSVG } from 'qrcode.react';
 
 export const ScanHistory =()=>{
 
     const data = JSON.parse(localStorage.getItem(SCAN_DATA)  || '[]');
-    console.log(data)
+    console.log(data);
+
     return(
         <div>
+            <h1 className={s.list_tittle}>History</h1>
+            <div className={s.list}>
             {data.map((text)=>(
-                <p>{text}</p>
+                <p key={text} className={s.list_elem}>
+                    {text}
+                    <QRCodeSVG value={text} size={100} />
+                </p>
             ))}
+            </div>
         </div>
     )
 }
